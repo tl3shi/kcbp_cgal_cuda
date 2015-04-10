0a26ed16c892219c863c1ed713d8c3b4aa5bdb65
tanglei <gems8@tanglei.name>
2015-04-10 20:13:22 +0800

    speedup

--- kcbp_cgal_cuda/Project1/LibCCDQuery2.hpp
+++ kcbp_cgal_cuda/Project1/LibCCDQuery2.hpp
@@ -5,6 +5,8 @@
 #include "ccd/ccd.h"
 #include "ccd/quat.h" // for work with quaternions
 
+#include <glm/glm.hpp>
+
 #ifdef _DEBUG
 #pragma comment(lib, "../Debug/ccd.lib")
 #else
@@ -22,26 +24,150 @@ struct CCD_Convex
     }
 };
 
- 
+//from PmCloth3D
+void Rotmat2Quar(const mat4 &rotMat, float &m_X, float &m_Y, float &m_Z, float &m_W)
+{
+    float fTrace = rotMat[0][0]+rotMat[1][1]+rotMat[2][2];
+    float fRoot;
+    if ( fTrace > 0.0f )
+    {
+        // |w| > 1/2, may as well choose w > 1/2
+        fRoot = sqrt(fTrace + 1.0f);  // 2w
+        m_W = 0.5f*fRoot;
+        fRoot = 0.5f/fRoot;  // 1/(4w)
+        m_X = (rotMat[2][1]-rotMat[1][2])*fRoot;
+        m_Y = (rotMat[0][2]-rotMat[2][0])*fRoot;
+        m_Z = (rotMat[1][0]-rotMat[0][1])*fRoot;
+    }
+    else
+    {
+        // |w| <= 1/2
+        static size_t s_iNext[3] = { 1, 2, 0 };
+        size_t i = 0;
+        if ( rotMat[1][1] > rotMat[0][0] )
+            i = 1;
+        if ( rotMat[2][2] > rotMat[i][i] )
+            i = 2;
+        size_t j = s_iNext[i];
+        size_t k = s_iNext[j];
+        fRoot = sqrt(rotMat[i][i]-rotMat[j][j]-rotMat[k][k] + 1.0f);
+        float* apkQuat[3] = { &m_X, &m_Y, &m_Z };
+        *apkQuat[i] = 0.5f*fRoot;
+        fRoot = 0.5f/fRoot;
+        m_W = (rotMat[k][j]-rotMat[j][k])*fRoot;
+        *apkQuat[j] = (rotMat[j][i]+rotMat[i][j])*fRoot;
+        *apkQuat[k] = (rotMat[k][i]+rotMat[i][k])*fRoot;
+    }
+} 
+
+mat4 Quar2Rotmat(float m_X, float m_Y, float m_Z, float m_W)
+{
+    float nQ = m_X*m_X + m_Y*m_Y + m_Z*m_Z + m_W*m_W;
+    float s = 0.0;
+
+    if (nQ > 0.0) {
+        s = 2.0f/nQ;
+    }
+
+    float xs = m_X*s;
+    float ys = m_Y*s;
+    float zs = m_Z*s;
+    float wxs = m_W*xs;
+    float wys = m_W*ys;
+    float wzs = m_W*zs;
+    float xxs = m_X*xs;
+    float xys = m_X*ys;
+    float xzs = m_X*zs;
+    float yys = m_Y*ys;
+    float yzs = m_Y*zs;
+    float zzs = m_Z*zs;
+
+    return mat4(1.0f-yys-zzs, xys-wzs, xzs + wys, 0,
+        xys + wzs, 1.0f-xxs-zzs, yzs-wxs, 0,
+        xzs-wys, yzs + wxs, 1.0f-xxs-yys, 0,
+        0, 0, 0, 1);
+}
+
+
 void supportConvex2(const void* obj, const ccd_vec3_t* dir_, ccd_vec3_t* v)
 {
     const CCD_Convex * c = static_cast<const CCD_Convex*>(obj);
     RealValueType maxdot = - RealValueTypeMax;
+  
+    /*
     CP_Vector3D dir(dir_->v[0], dir_->v[1], dir_->v[2]);
     CP_Vector3D newP;
+    int max_index = 0;
     for(int i = 0; i < c->points.size(); i++)
     {
         CP_Vector3D curp = c->transformMatrix * c->points[i];
         RealValueType dot = curp * dir;
         if(dot > maxdot)
         {
-            newP = curp;
+            newP = curp; 
             maxdot = dot;
+            max_index = i;
         }
     }
     ccdVec3Set(v, newP.x, newP.y, newP.z);
+    */    
+       /* //Attemp1 ---Wrong
+       {
+           mat4 transform = c->transformMatrix ;
+           //CP_Vector3D translate(transform[0][3], transform[1][3], transform[2][3]);
+           //col major
+           glm::detail::tmat3x3<RealValueType> glmMatrix(transform[0][0], transform[1][0], transform[2][0], 
+                                          transform[0][1], transform[1][1], transform[2][1], 
+                                          transform[0][2], transform[1][2], transform[2][2] );
+           glm::detail::tmat3x3<RealValueType> imatrix = glmMatrix._inverse();
+           glm::detail::tvec3<RealValueType> dir(dir_->v[0], dir_->v[1], dir_->v[2]);
+           dir = imatrix * dir;
+           int max_index = 0;
+           CP_Vector3D newDir(dir.x, dir.y, dir.z);
+           maxdot = - RealValueTypeMax;
+           for(int i = 0; i < c->points.size(); i++)
+           {
+               RealValueType dot = c->points[i] * newDir;
+               if(dot > maxdot)
+               {
+                   maxdot = dot;
+                   max_index = i;
+               }
+           }
+           CP_Vector3D newP = transform * c->points[max_index];
+           ccdVec3Set(v, newP.x, newP.y, newP.z);
+       }
+       */
+       //Attemp2, Right
+       {
+            float xyzw[4];
+            Rotmat2Quar(c->transformMatrix, xyzw[0], xyzw[1], xyzw[2], xyzw[3]);
+            ccd_quat_t src_;
+            ccd_quat_t dest;
+            ccdQuatSet(&src_, xyzw[0], xyzw[1], xyzw[2], xyzw[3]);
+            ccdQuatInvert2(&dest, &src_);
+            mat4 tmpM = Quar2Rotmat(dest.q[0], dest.q[1], dest.q[2], dest.q[3]); //this matrix'3X3 shoule be same as imatrix up(attemp1) ===> same
+            ccd_vec3_t newDirCCD;
+            ccdVec3Copy(&newDirCCD,  dir_);
+            ccdQuatRotVec(&newDirCCD, &dest);
+            int max_index = 0;
+            maxdot = - RealValueTypeMax;
+            CP_Vector3D newDir(newDirCCD.v[0], newDirCCD.v[1], newDirCCD.v[2]);//same as ? newDir up(attemp1)  ===¡· no
+            for(int i = 0; i < c->points.size(); i++)
+            {
+                RealValueType dot = c->points[i] * newDir;
+                if(dot > maxdot)
+                {
+                    maxdot = dot;
+                    max_index = i;
+                }
+            }
+            CP_Vector3D newP = c->transformMatrix * c->points[max_index];
+            ccdVec3Set(v, newP.x, newP.y, newP.z);
+       }
 }
- 
+
+
 struct LibCCDQuery2: public ICollisionQuery
 {
 
@@ -52,14 +178,6 @@ struct LibCCDQuery2: public ICollisionQuery
         
         return intersect == 0 ? false : true;
     }
-
-    //wrong....
-    bool detection(const vec3 &axis, const int jiaodu, const vec3 &translate)
-    {
-       assert(false);
-       return true;
-    }
-
 
 private:
     
